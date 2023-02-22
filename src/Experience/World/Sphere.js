21593f9f786f08ef205fa8033379beb65b3d4699
Ishiban10 <marinishaan@gmail.com>
2023-02-22 13:37:45 -0800

    trying to remove sushi again

--- src/Experience/World/Sphere.js
+++ src/Experience/World/Sphere.js
@@ -6,7 +6,7 @@ export default class Sphere {
         this.experience = new Experience()
         this.scene = this.experience.scene
         this.resources = this.experience.resources
-        this.resource = this.resources.items.sushiModel
+        //this.resource = this.resources.items.sushiModel
 
         this.model = this.resource.scene
         this.model.scale.set(1,1,1)
